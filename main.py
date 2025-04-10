6b40a0e352d73c487034296b3f555e07ff8f8289
EnochCHIU <23034597d@connect.polyu.hk>
2025-04-10 11:20:02 +0000

    scroll top (fix)

--- main.py
+++ main.py
@@ -447,9 +447,8 @@ page = st.session_state.page
 # Main content area
 with st.container():
     if page == "main":
-        #scroll to the top
-        st.markdown("<style>body {scroll-behavior: smooth;}</style>", unsafe_allow_html=True)
-        st.markdown("<script>window.scrollTo(0, 0);</script>", unsafe_allow_html=True)
+        #scroll to the top by streamlit
+        st.markdown('<script>window.scrollTo(0, 0);</script>', unsafe_allow_html=True)
         show_main()
     elif page == "Personal Info":
         show_personal_info()
